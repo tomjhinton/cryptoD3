e2486bfb73fa98ab4d75a799dfe91e4559050b66
Tom Hinton <tomjhinton@gmail.com>
2019-11-18 15:13:04 +0000

    bars on x axis correctly

--- src/components/Home.js
+++ src/components/Home.js
@@ -70,7 +70,7 @@ class Home extends React.Component{
     //     svgCanvas.append("g")
        //.call(d3.axisLeft('y'));
 
-       var width = 1200, height = 1800;
+       var width = 1400, height = 600;
 
        var dataNum = data.map(x=> x= x.RAW.USD.PRICE)
        var svg = d3.select("body")
@@ -98,24 +98,91 @@ class Home extends React.Component{
 
        var xAxisTranslate = height/2 + 10;
 
-           svg.append("g")
-                   .attr("transform", "translate(50, " + xAxisTranslate  +")")
-                   .call(x_axis)
+    svg.append("g")
+      .attr("transform", "translate(50, " + xAxisTranslate  +")")
+      .call(x_axis)
 
-
-    svg.selectAll('rect')
+      console.log(yscale)
+    svg.selectAll('bar')
       .data(data).enter()
       .append('rect')
       .attr('width', 40)
       .attr('height', (datapoint) => datapoint.RAW.USD.PRICE)
       .attr('fill', 'blue')
-      .attr('x', (datapoint, iteration) => iteration * 65)
-      .attr('y', (datapoint) => height - datapoint.RAW.USD.PRICE * yscale)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr('x', (datapoint, iteration) => (iteration * 65) +60)
+      .attr('y', (dataPoint, i) => height/2 + 10 - dataPoint.RAW.USD.PRICE )
+
+      // .append("g")
+      // .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 
   }
+
+
+  chart2(data){
+    const  canvas = document.getElementById('canvas')
+    var svg = d3.select(canvas),
+        margin = 200,
+        width = svg.attr("width") - margin,
+        height = svg.attr("height") - margin
+
+    svg.append("text")
+       .attr("transform", "translate(100,0)")
+       .attr("x", 50)
+       .attr("y", 50)
+       .attr("font-size", "24px")
+       .text("XYZ Foods Stock Price")
+
+    var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+        yScale = d3.scaleLinear().range([height, 0]);
+
+    var g = svg.append("g")
+               .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+    d3.csv("XYZ.csv", function(error, data) {
+        if (error) {
+            throw error;
+        }
+
+        xScale.domain(data.map(function(d) { return d.year; }));
+        yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(xScale))
+         .append("text")
+         .attr("y", height - 250)
+         .attr("x", width - 100)
+         .attr("text-anchor", "end")
+         .attr("stroke", "black")
+         .text("Year");
+
+        g.append("g")
+         .call(d3.axisLeft(yScale).tickFormat(function(d){
+             return "$" + d;
+         })
+         .ticks(10))
+         .append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 6)
+         .attr("dy", "-5.1em")
+         .attr("text-anchor", "end")
+         .attr("stroke", "black")
+         .text("Stock Price");
+
+        g.selectAll(".bar")
+         .data(data)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d.year); })
+         .attr("y", function(d) { return yScale(d.value); })
+         .attr("width", xScale.bandwidth())
+         .attr("height", function(d) { return height - yScale(d.value); });
+    });
+
+  }
+
+
 
 
   componentDidUpdate() {
